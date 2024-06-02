464fb72bd9ab41bff3d35d648f39224c990ba23f
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-06-02 09:51:57 +0300

    Add Identity and EF support.

--- IdentityDemo.WebApi/Controllers/WeatherForecastController.cs
+++ IdentityDemo.WebApi/Controllers/WeatherForecastController.cs
@@ -1,7 +1,9 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityDemo.WebApi.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
--- IdentityDemo.WebApi/Program.cs
+++ IdentityDemo.WebApi/Program.cs
@@ -1,12 +1,33 @@
+using IdentityDemo.WebApi.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseInMemoryDatabase("AppDb"));
+
+builder.Services.AddAuthorization();
+// </snippetAddAuthorization>
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
+app.MapIdentityApi<IdentityUser>();
+
 // Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
